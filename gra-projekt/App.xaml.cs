d484acc928ee40be9ca9fffe4dd3634548d8b34a
KrzysztofLata <latakrzysztofsp15@gmail.com>
2026-02-09 14:14:58 +0100

    dodane pliki xaml.cs

---
+++ gra-projekt/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace gra_projekt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ gra-projekt/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace gra_projekt
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
